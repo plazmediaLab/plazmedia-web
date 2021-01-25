0ecc00bcaf2a8ef2b8349e66d68a38bdd25a9ead
AdrianGD1337 <contacto.plazmedia@gmail.com>
2021-01-24 21:22:40 -0600

    [MODIFY] Remplazo de API nativa Next js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import Layout from 'components/layout';
 import HomeMainContent from 'components/home';
-import AppContext from 'context/AppContext/AppContext';
-import { useContext, useEffect } from 'react';
+// import AppContext from 'context/AppContext/AppContext';
+// import { useContext, useEffect } from 'react';
 
 function Home({ skills }) {
-  const appContext = useContext(AppContext);
-  const { setSkillsMethod } = appContext;
+  // const appContext = useContext(AppContext);
+  // const { setSkillsMethod } = appContext;
 
-  useEffect(() => {
-    if (skills) {
-      setSkillsMethod(skills);
-    }
-  }, [skills]);
+  // useEffect(() => {
+  //   if (skills) {
+  //     setSkillsMethod(skills);
+  //   }
+  // }, [skills]);
 
   return (
     <Layout>
@@ -20,13 +20,13 @@ function Home({ skills }) {
   );
 }
 
-Home.getInitialProps = async () => {
-  const res = await fetch(`http://localhost:3000/api/skills`);
-  const json = await res.json();
+// Home.getInitialProps = async () => {
+//   const res = await fetch(`http://localhost:3000/api/skills`);
+//   const json = await res.json();
 
-  return {
-    skills: json ? json : []
-  };
-};
+//   return {
+//     skills: json ? json : []
+//   };
+// };
 
 export default Home;
