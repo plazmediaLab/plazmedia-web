5b5b30075a4e2b860be8c04dc460a6a6a1dcc0d2
AdrianGD1337 <contacto.plazmedia@gmail.com>
2021-01-28 15:19:26 -0600

    [ADD] test

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
 import Layout from 'components/layout';
 import HomeMainContent from 'components/home';
-import AppContext from 'context/AppContext/AppContext';
-import { useContext, useEffect } from 'react';
 
-function Home({ skills }) {
-  const appContext = useContext(AppContext);
-  const { setSkillsMethod } = appContext;
-
-  useEffect(() => {
-    setSkillsMethod(skills);
-  }, []);
-
+function Home() {
   return (
     <Layout>
       <HomeMainContent />
@@ -18,18 +9,18 @@ function Home({ skills }) {
   );
 }
 
-export async function getStaticProps() {
-  console.log('');
-  const res = await fetch(
-    `${process.env.PRODUCTION ? process.env.PROD_API_HOST : process.env.LOCAL_API_HOST}/skills`
-  );
-  const json = await res.json();
+// export async function getStaticProps() {
+//   console.log('');
+//   const res = await fetch(
+//     `${process.env.PRODUCTION ? process.env.PROD_API_HOST : process.env.LOCAL_API_HOST}/skills`
+//   );
+//   const json = await res.json();
 
-  return {
-    props: {
-      skills: json
-    }
-  };
-}
+//   return {
+//     props: {
+//       skills: json
+//     }
+//   };
+// }
 
 export default Home;
